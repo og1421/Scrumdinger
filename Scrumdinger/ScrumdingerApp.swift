e0e2e93bf116c18efa89c7fafb216b9fb5e22ddc
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-03-14 13:34:13 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Orlando Moraes Martins on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
