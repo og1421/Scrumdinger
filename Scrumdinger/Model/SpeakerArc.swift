854113187522a8dbaebd1e481f9d912f297769d1
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-03-30 16:14:11 -0300

    Drawing the progress line

---
+++ Scrumdinger/Model/SpeakerArc.swift
@@ -0,0 +1,36 @@
+//
+//  SpeakerArc.swift
+//  Scrumdinger
+//
+//  Created by Orlando Moraes Martins on 30/03/23.
+//
+
+import SwiftUI
+
+struct SpeakerArc: Shape {
+    //MARK: - Properties
+    let speakerIndex: Int
+    let totalSpeakers: Int
+    
+    private var degreesPerSpeaker: Double {
+        360.0 / Double(totalSpeakers)
+    }
+    
+    private var startAngle: Angle {
+        Angle(degrees: degreesPerSpeaker * Double(speakerIndex) + 1.0)
+    }
+    
+    private var endAngle: Angle {
+        Angle(degrees: startAngle.degrees + degreesPerSpeaker - 1.0)
+    }
+    
+    //MARK: - returning function
+    func path(in rect: CGRect) -> Path {
+        let diameter = min(rect.size.width, rect.size.height) - 24.0
+        let radius = diameter / 2.0
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        return Path { path in
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        }
+    }
+}
